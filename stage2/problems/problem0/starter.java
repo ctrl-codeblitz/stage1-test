8502e5e081e516cebac9119474eb6801514f4c37
Aaron Cayanan <127135170+aacayanan@users.noreply.github.com>
2026-03-19 20:27:46 -0700

    Initial problem set setup

---
+++ stage2/problems/problem0/starter.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class starter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // --- Input reading ---
+        String s = sc.next();
+
+        // --- Solution ---
+        // The following variables are available:
+        // s: String
+
+        // TODO: Implement the solution
+        
+        sc.close();
+    }
+}
